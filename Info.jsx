cf2da01c99bda3c380af1abc4285ab899639538e
Alfred Francis <60069653+alfredfrancis68@users.noreply.github.com>
2021-06-26 17:32:41 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div className="note">
+      <h1>Javscript and React.js</h1>
+      <p>A basicwebDev React.js Bootcamp.</p>
+    </div>
+  );
+}
+
+export default App;
