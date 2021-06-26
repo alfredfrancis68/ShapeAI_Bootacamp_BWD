1236ff6773a8060e2123988fcb214319b251ea21
Alfred Francis <60069653+alfredfrancis68@users.noreply.github.com>
2021-06-26 17:46:40 +0530

    Update Info.jsx

--- Info.jsx
+++ Info.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="note">
       <h1>Javscript and React.js</h1>
-      <p>A basicwebDev React.js Bootcamp.</p>
+      <p>This was an amazing bootcamp taken up by Shaurya Sinha. We covered everything from scratch including Javscript,React.js,HTML</p>
     </div>
   );
 }
